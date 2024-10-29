a767bc0bd96ff536662186e75f6fdca2ba06a577
PIPIKAI <z1652091948@outlook.com>
2024-10-29 11:07:21 +0800

    Balatro-CommunityPack

---
+++ Balatro-CommunityPack/localization/zh_CN.lua
@@ -0,0 +1,109 @@
+return {
+    descriptions = {
+        Joker = {
+            j_bcp_ultimace = {
+                name = "至尊A",
+                text = {
+                  "重新触发",
+                  "计分的 {C:attention}增强 A{}",
+                }
+            },
+            j_bcp_baba = {
+                name = "霸霸",
+                text = {
+                  "如果出牌包括一个",
+                  "{C:attention}#2#{} 或 {C:attention}#3#{}",
+                  " {X:red,C:white} X#1# {} 倍率"
+              }
+            },
+            j_bcp_missingtexture = {
+                name = "丢失材质",
+                text = {
+                  "{C:attention}两队{} 被计分为",
+                  "{C:attention}四条{}"
+              }
+            },
+            j_bcp_background = {
+                name = "背景图片",
+                text = {
+                  "倍率等于出牌次数的平方",
+                  "这可以从你的计分卡中得出"
+              }
+            },
+            j_bcp_inverted = {
+                name = "倒转小丑",
+                text = {
+                  "三回合后",
+                  "卖掉此牌增加 ",
+                  "一个免费的 {C:attention}负片标签{}"
+              }
+            },
+            j_bcp_freesample = {
+                name = "免费样本",
+                text = {
+                  "如果出牌类型是{C:attention}#1#{}， +1 手牌 ",
+                  "每回合改变类型"
+              }
+            },
+            j_bcp_notfound = {
+                name = "404 小丑",
+                text = {
+                  "{C:attention}#1#{} / {C:attention}#2#{} 几率让筹码变为{C:chips}#3#{}" -- should be set chips to 404, but that's hard to do
+              }
+            },
+            j_bcp_executioner = {
+                name = "刽子手",
+                text = {
+                  "如果这回合第一次出牌类型是 {C:attention}#1#{},",
+                  "摧毁所有计分的牌",
+                  "类型每回合都会改变"
+              }
+            },
+            j_bcp_passport = {
+                name = "护照",
+                text = {
+                  "Gives benefits of joker to the right",
+                  "{C:attention}无视{} 条件",
+                  "{C:attention}#1#{}"
+              }
+            },
+            j_bcp_stone_mask = {
+                name = "石像鬼面具(Wryyyyyyyyyyyyy",
+                text = {
+                  "石头牌计分时增加一个随机的增强"
+              }
+            },
+            j_bcp_clown_pennywise = {
+                name = "Stone Mask",
+                text = {
+                  "如果第一回合计分的手牌包含",
+                  "2, 3, 4 或 5,",
+                  "每个增加{C:mult}+#1# 倍率{} 并摧毁他们",
+                  "(当前 {C:mult}+#2# 倍率{})"
+              }
+            },
+        },
+        Other = {
+            creator_revenge = {
+                name = "Creator",
+                text = { "复仇" }
+            },
+            creator_sairya = {
+                name = "Creator",
+                text = { "盖亚!!!!" }
+            },
+            creator_mysticsailbo = {
+                name = "Creator",
+                text = { "神秘帆船" }
+            },
+            creator_deepdawn = {
+                name = "Creator",
+                text = { "黑暗黎明之主" }
+            },
+            creator_assassinchara = {
+                name = "Creator",
+                text = { "刺客查拉" }
+            },
+        },
+    },
+}
